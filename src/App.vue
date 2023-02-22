d669205348f6393ed3d1e2ed064ef417959c5749
sunshine <sunshineboy085@gmail.com>
2023-02-22 09:14:02 +0800

    add:001RotateAndColorChange

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main">
+    <div class="title">
+      <RotateAndColorChange></RotateAndColorChange>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import RotateAndColorChange from './views/RotateAndColorChange.vue';
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  list-style: none;
+  width: 100%;
+  height: 100%;
+}
+</style>
